d6657a997a90219e0bb27a9e2d4615c48cb5ed63
BelisariusMC <106339357+BelisariusMC@users.noreply.github.com>
2022-07-31 21:45:09 -0300

    Operations description is now optional

--- dirhandler.py
+++ dirhandler.py
@@ -16,41 +16,71 @@ from distutils import dir_util
 class DirHandler:
 
     @staticmethod
-    def remove_dir(path, description):
+    def remove_dir(path, description=None, debug=False):
+        if debug is True and description is None:
+            description = path
+
         if os.path.isdir(path):
             dir_util.remove_tree(path)
-            print("Deleted " + description)
+            if debug is True:
+                print("Deleted " + description)
         else:
-            print("Skipped " + description + " deletion, didn't exist")
+            if debug is True:
+                print("Skipped " + description + " deletion, didn't exist")
 
     @staticmethod
-    def remove_file(path, description):
+    def remove_file(path, description=None, debug=False):
+        if debug is True and description is None:
+            description = path
+
         if os.path.isfile(path):
             os.remove(path)
-            print("Deleted " + description)
+            if debug is True:
+                print("Deleted " + description)
         else:
-            print("Skipped " + description + " deletion, didn't exist")
+            if debug is True:
+                print("Skipped " + description + " deletion, didn't exist")
 
     @staticmethod
-    def copy_dir(from_path, to_path, from_desc, to_desc):
+    def copy_dir(from_path, to_path, from_desc=None, to_desc=None, debug=False):
+        if debug is True:
+            if from_desc is None:
+                from_desc = from_path
+            if to_desc is None:
+                to_desc = to_path
+
         if os.path.isdir(from_path):
             dir_util.copy_tree(from_path, to_path)
-            print("Copied " + from_desc + " to " + to_desc)
+            if debug is True:
+                print("Copied " + from_desc + " to " + to_desc)
         else:
-            print("Skipped " + from_desc + " copying to " + to_desc + ", didn't exist")
+            if debug is True:
+                print("Skipped " + from_desc + " copying to " + to_desc + ", didn't exist")
 
     @staticmethod
-    def copy_file(from_path, to_path, from_desc, to_desc):
+    def copy_file(from_path, to_path, from_desc=None, to_desc=None, debug=False):
+        if debug is True:
+            if from_desc is None:
+                from_desc = from_path
+            if to_desc is None:
+                to_desc = to_path
+
         if os.path.isfile(from_path):
             shutil.copy2(from_path, to_path)
-            print("Copied " + from_desc + " to " + to_desc)
+            if debug is True:
+                print("Copied " + from_desc + " to " + to_desc)
         else:
-            print("Skipped " + from_desc + " copying to " + to_desc + ", didn't exist")
+            if debug is True:
+                print("Skipped " + from_desc + " copying to " + to_desc + ", didn't exist")
 
     @staticmethod
-    def rename_file(path, name):
-        DirHandler.copy_file(path, os.path.dirname(path) + '/' + name,
-                             path, os.path.dirname(path) + '/' + name)
-        DirHandler.remove_file(path, path)
+    def rename_file(path, name, description=None, debug=False):
+        if description is None:
+            description = path + " to " + name
 
-# ==============================================================================
+        DirHandler.copy_file(path, os.path.dirname(path) + '/' + name, debug=debug)
+        DirHandler.remove_file(path, debug=debug)
+        if debug is True:
+            print("Renamed " + description)
+
+    # ==============================================================================
