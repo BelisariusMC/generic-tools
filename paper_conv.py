220dc9c698d0d040f3ae8eed2a4e73b2bc94eabb
BelisariusMC <106339357+BelisariusMC@users.noreply.github.com>
2022-08-01 18:41:30 -0300

    Adequate to new dirhandler syntax

--- paper_conv.py
+++ paper_conv.py
@@ -17,16 +17,16 @@ class PaperWorld:
         self.world_vanilla_path = world_vanilla_path
 
     def paper_to_vanilla(self):
-        DirHandler.copy_dir(self.world_paper_path, self.world_vanilla_path,
-                            "Paper world", "Vanilla world")
-        DirHandler.copy_dir(self.world_paper_path + "_nether/DIM-1",
-                            self.world_vanilla_path + "/DIM-1",
-                            "Paper world_nether", "Vanilla world")
-        DirHandler.copy_dir(self.world_paper_path + "_the_end/DIM1",
-                            self.world_vanilla_path + "/DIM1",
-                            "Paper world_the_end", "Vanilla world")
-        DirHandler.remove_file(self.world_vanilla_path + "/paper-world.yml", "Paper world config")
-        DirHandler.remove_dir(self.world_vanilla_path + '/datapacks/bukkit', "Bukkit datapack")
+        DirHandler.copy(self.world_paper_path, self.world_vanilla_path,
+                        "Paper world", "Vanilla world")
+        DirHandler.copy(self.world_paper_path + "_nether/DIM-1",
+                        self.world_vanilla_path + "/DIM-1",
+                        "Paper world_nether", "Vanilla world")
+        DirHandler.copy(self.world_paper_path + "_the_end/DIM1",
+                        self.world_vanilla_path + "/DIM1",
+                        "Paper world_the_end", "Vanilla world")
+        DirHandler.remove(self.world_vanilla_path + "/paper-world.yml", "Paper world config")
+        DirHandler.remove(self.world_vanilla_path + '/datapacks/bukkit', "Bukkit datapack")
 
         print("Converted Paper worlds " + self.world_paper_path + ", " + self.world_paper_path + "_nether, " +
               self.world_paper_path + "_the_end" + " to Vanilla world " + self.world_vanilla_path)
@@ -35,14 +35,14 @@ class PaperWorld:
         """ Note: Paper handles this automatically when migrating from Vanilla """
         print("Note: Paper handles this automatically when migrating from Vanilla")
 
-        DirHandler.copy_dir(self.world_vanilla_path, self.world_paper_path,
-                            "Vanilla world", "Paper world")
-        DirHandler.copy_dir(self.world_vanilla_path + "/DIM-1",
-                            self.world_paper_path + "_nether/DIM-1",
-                            "Vanilla world nether", "Paper world_nether")
-        DirHandler.copy_dir(self.world_vanilla_path + "/DIM1",
-                            self.world_paper_path + "_the_end/DIM1",
-                            "Vanilla world the end", "Paper world_the_end")
+        DirHandler.copy(self.world_vanilla_path, self.world_paper_path,
+                        "Vanilla world", "Paper world")
+        DirHandler.copy(self.world_vanilla_path + "/DIM-1",
+                        self.world_paper_path + "_nether/DIM-1",
+                        "Vanilla world nether", "Paper world_nether")
+        DirHandler.copy(self.world_vanilla_path + "/DIM1",
+                        self.world_paper_path + "_the_end/DIM1",
+                        "Vanilla world the end", "Paper world_the_end")
 
         print("Converted Vanilla world " + self.world_vanilla_path + " to Paper worlds " +
               self.world_paper_path + ", " + self.world_paper_path + "_nether, " + self.world_paper_path + "_the_end")
