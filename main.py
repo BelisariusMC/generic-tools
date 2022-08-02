e91528f0b0c5a31f7741c201efef4e6d8e9abcac
BelisariusMC <106339357+BelisariusMC@users.noreply.github.com>
2022-08-01 21:32:49 -0300

    Update comments

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# ==============================================================================
+# =============================================================================
 # Created By  : BelisariusMC
 # Created Date: 19 jun. 2022
-# Version     : 1.0.3
+# Version     : 1.0.4
 # License     : BSD 3-Clause "New" or "Revised" License
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Generic tools to help development and maintenance of Minecraft mods, modpacks
 # and servers. Included features:
-# > Excel files to Markdown (.md) tables converter
-# > Paper world format to Vanilla format (and vice-versa)
-# ==============================================================================
+# > Convert Excel/Sheets files to Markdown (.md) tables
+# > Convert Paper worlds to Vanilla (and vice-versa)
+# =============================================================================
 __author__ = "BelisariusMC"
 __copyright__ = "Copyright 2022, BelisariusMC"
 __license__ = "BSD 3-Clause \"New\" or \"Revised\" License"
-__version__ = "1.0.3"
-# ==============================================================================
+__version__ = "1.0.4"
+# =============================================================================
 import os
 
 from excel_conv import ExcelMarkdown
 from paper_conv import PaperWorld
-# ==============================================================================
+# =============================================================================
 print('# ' + '=' * 77)
+print("                __  __  ___    ___ _    ___   _____         _    \n"
+      "               |  \/  |/ __|  / __| |  |_ _| |_   _|__  ___| |___\n"
+      "               | |\/| | (__  | (__| |__ | |    | |/ _ \/ _ \ (_-<\n"
+      "               |_|  |_|\___|  \___|____|___|   |_|\___/\___/_/__/\n")
+print("  Simple command line interface (CLI) tools to help development and maintenance \n"
+      "                     of Minecraft mods, modpacks and servers")
+print('# ' + '-' * 77)
+# -----------------------------------------------------------------------------
 print('Author: ' + __author__)
 print('Copyright: ' + __copyright__)
 print('License: ' + __license__)
 print('Version: ' + __version__)
 print('# ' + '=' * 77)
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # TODO: Export system for modpack development from an usable instance
 # TODO: modpack conversion (Packwiz, MultiMC, CurseForge, Modrinth)
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 """
 Example to convert the Technical Enhanced 1.18.2 mod list from .xlsx to .md
 
@@ -42,7 +50,7 @@ infos on the final .md table.
 modlist_path = os.getcwd() + "/example_excel_to_md/MODLIST.xlsx"
 ExcelMarkdown(modlist_path).to_markdown(skiprows=5, header=None, usecols=range(0, 6))
 # excel_to_md('MODLIST.xlsx').to_markdown(skiprows=5, header=None, usecols=range(0, 6))
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 """ Example to convert a Paper world into a Vanilla world """
 
 root_path = os.getcwd() + "/example_paper_to_vanilla"
@@ -50,4 +58,4 @@ world_paper_path = root_path + "/world_paper"
 world_vanilla_path = root_path + "/world_vanilla"
 
 PaperWorld(world_paper_path, world_vanilla_path).paper_to_vanilla()
-# ==============================================================================
+# =============================================================================
